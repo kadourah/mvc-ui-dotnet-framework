bf3361458f9656df51cbb9d6d85cb66443e77c51
kadourah-pivotal <42582592+kadourah-pivotal@users.noreply.github.com>
2022-02-02 15:17:42 -0500

    Add project files.

---
+++ mvc-ui-dotnet-framework/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_ui_dotnet_framework
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
